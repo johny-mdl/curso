6d7d6c7b20dafad4f01d380a94ec4fe66c48b41a
Johny <johny.mdl.pt@gmail.com>
2018-08-23 23:43:18 +0100

    objeto categoria

--- src/main/java/com/curso/resources/CategoriaResource.java
+++ src/main/java/com/curso/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Está a funcionar";
+	public List<Categoria> listar() {
+		Categoria categoria1 = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Cenas");
+
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+
+		return categorias;
 	}
 
 }
