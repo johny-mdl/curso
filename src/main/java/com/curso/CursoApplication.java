81804604d9b506ecd04fe23c0e5d5d1b1000d422
Johny <johny.mdl.pt@gmail.com>
2018-08-24 00:35:07 +0100

    inserir registos ao lançar app

--- src/main/java/com/curso/CursoApplication.java
+++ src/main/java/com/curso/CursoApplication.java
@@ -1,12 +1,30 @@
 package com.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.curso.domain.Categoria;
+import com.curso.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "cenas");
+		Categoria cat2 = new Categoria(null, "pow");
+
+		repo.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
