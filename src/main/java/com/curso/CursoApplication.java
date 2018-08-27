20a3fc9c1cf7ce47dce9311301fa9958bfb4b887
Johny <johny.mdl.pt@gmail.com>
2018-08-27 23:30:57 +0100

    criar cliente e endereço

--- src/main/java/com/curso/CursoApplication.java
+++ src/main/java/com/curso/CursoApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.domain.Categoria;
 import com.curso.domain.Cidade;
+import com.curso.domain.Cliente;
+import com.curso.domain.Endereco;
 import com.curso.domain.Estado;
 import com.curso.domain.Produto;
+import com.curso.domain.enums.TipoCliente;
 import com.curso.repositories.CategoriaRepository;
 import com.curso.repositories.CidadeRepository;
+import com.curso.repositories.ClienteRepository;
+import com.curso.repositories.EnderecoRepository;
 import com.curso.repositories.EstadoRepository;
 import com.curso.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursoApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository repoCidade;
+
+	@Autowired
+	private ClienteRepository repoCliente;
+
+	@Autowired
+	private EnderecoRepository repoEndereco;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
@@ -65,5 +76,17 @@ public class CursoApplication implements CommandLineRunner {
 		repoProduto.saveAll(Arrays.asList(p1, p2, p3));
 		repoEstado.saveAll(Arrays.asList(est1, est2));
 		repoCidade.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "email", "912781904", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("23432432", "2454235243"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apt 300", "jardim", "4324213", cli1, c1);
+
+		Endereco e2 = new Endereco(null, "Rua Flores2", "400", "Apt 400", "jardim2", "43242132", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		repoCliente.saveAll(Arrays.asList(cli1));
+		repoEndereco.saveAll(Arrays.asList(e1, e2));
 	}
 }
